b90931f5b856544be38b5d5aa15549746365bf11
marsel <marsel_idrisov2001@yahoo.com>
2022-08-12 21:35:40 +0300

    Made task 13, but do not understand

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//   что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+char[] arr = number.ToString().ToCharArray();
+
+if (number >= 100)
+{
+    Console.WriteLine($"Третья цифра заданного числа - {arr[2]}");
+}
+else
+{
+    Console.WriteLine("Wrong. Третьей цифры нет: введите трёхзначное число.");
+}
